515efbd3b4de05a09fb5438a19ef5687bd9e8cb5
Cassiano <cassianocampos2203@gmail.com>
2025-06-13 12:25:54 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import pkg from '@prisma/client'
+const { PrismaClient } = pkg
+
+
+const prisma = new PrismaClient()
+
+const app = express() 
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', async (req, res) => {
+   
+   await prisma.user.create ({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+
+    res.status(201).json(req.body)
+}) 
+
+app.get('/usuarios', (req, res) => {
+
+    res.status(200).json(users)
+})
+
+app.listen(3000)
