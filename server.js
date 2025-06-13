41cb353e21062e267df91085bb7da3c419c6e587
CassianoCampos013 <cassianocampos2203@gmail.com>
2025-06-13 20:39:51 -0300

    delete adicionado

--- server.js
+++ server.js
@@ -48,6 +48,16 @@ app.put('/usuarios/:id', async (req, res) => {
       res.status(201).json(req.body)
 })
 
+app.delete('/usuarios/:id', async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  })
+  
+  res.status(200).json({ message: 'Usuário deletado com sucesso!' })
+})
+
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000')
 })
